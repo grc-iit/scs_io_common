c7b280c9bf2f999eab30c30d714d30043d6c5bc9
Hariharan Devarajan <mani.hariharan@gmail.com>
2020-09-14 17:17:02 -0500

    initial commit

---
+++ include/common/class_loader.h
@@ -0,0 +1,14 @@
+//
+// Created by mani on 9/14/2020.
+//
+
+#ifndef RHEA_CLASS_LOADER_H
+#define RHEA_CLASS_LOADER_H
+
+
+class class_loader {
+
+};
+
+
+#endif //RHEA_CLASS_LOADER_H
---
+++ src/common/class_loader.cpp
@@ -0,0 +1,5 @@
+//
+// Created by mani on 9/14/2020.
+//
+
+#include "../../include/common/class_loader.h"
