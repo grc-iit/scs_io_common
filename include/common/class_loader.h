ec5f3d6a3cd1f263ea03ab5e133dda8c10b38b8e
Jie Ye <jye20@hawk.iit.edu>
2020-09-16 11:41:11 -0500

    Add the class_loader interface

--- include/common/class_loader.h
+++ include/common/class_loader.h
@@ -5,9 +5,21 @@
 #ifndef COMMON_CLASS_LOADER_H
 #define COMMON_CLASS_LOADER_H
 
+#include <basket/common/singleton.h>
 
-class class_loader {
 
+class ClassLoader {
+public:
+    ClassLoader(){}
+
+    void Load();
+
+    std::shared_ptr<Job> LoadJob(uint32_t job_id_);
+
+    std::shared_ptr<Task> LoadTask(uint32_t job_id_, uint32_t task_id_);
+
+private:
+    void* m_job_handler;
 };
 
 
