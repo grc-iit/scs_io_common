a204873c4c3e1d8344db213716cb79380bb96e1d
Hariharan Devarajan <mani.hariharan@gmail.com>
2020-09-16 20:09:57 -0500

    fixed class loader logic

--- include/common/class_loader.h
+++ include/common/class_loader.h
@@ -10,6 +10,7 @@
 #include <dlfcn.h>
 #include <common/configuration_manager.h>
 #include <common/error_codes.h>
+#include <boost/filesystem.hpp>
 
 class ClassLoader {
 public:
@@ -17,21 +18,32 @@ public:
 
     template<typename T>
     std::shared_ptr<T> LoadClass(uint32_t class_id_) {
-        m_job_handler = dlopen(COMMON_CONF->JOB_PATH.c_str(), RTLD_LAZY);
-        dlerror(); // clear error code
-        // find Job by job_id within the memory so
-        std::string job_symbol = "create_job_" + std::to_string(class_id_);
-        std::shared_ptr<T> (*create_job_fun)();
-        create_job_fun = (std::shared_ptr<T> (*)())dlsym(m_job_handler, job_symbol.c_str());
-        const char *dlsym_error = NULL;
-        if ((dlsym_error = dlerror()) != NULL){
-            //throw exception
-            return NULL;
-            //throw ErrorException(UNDEFINED_ENV_VARIABLE);
-        } else {
-            // create Job instance
-            return create_job_fun();
+        boost::filesystem::recursive_directory_iterator iter(COMMON_CONF->JOB_PATH);
+        boost::filesystem::recursive_directory_iterator end;
+        while (iter != end)
+        {
+            error_code ec;
+            // Increment the iterator to point to next entry in recursive iteration
+            iter.increment(ec);
+            if (ec) {
+                m_job_handler = dlopen(iter->path().string().c_str(), RTLD_LAZY);
+                dlerror(); // clear error code
+                // find Job by job_id within the memory so
+                std::string job_symbol = "create_job_" + std::to_string(class_id_);
+                std::shared_ptr<T> (*create_job_fun)();
+                create_job_fun = (std::shared_ptr<T> (*)())dlsym(m_job_handler, job_symbol.c_str());
+                const char *dlsym_error = NULL;
+                if ((dlsym_error = dlerror()) != NULL){
+                    continue
+                } else {
+                    // create Job instance
+                    return create_job_fun();
+                }
+            }
         }
+        //TODO:throw exception
+        return NULL;
+
     }
 
 private:
