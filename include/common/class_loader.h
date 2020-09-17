4a261bc130191579988f4d91ab89cb0deafba78d
Hariharan Devarajan <mani.hariharan@gmail.com>
2020-09-16 19:54:28 -0500

    build fix

--- include/common/class_loader.h
+++ include/common/class_loader.h
@@ -16,7 +16,23 @@ public:
     ClassLoader(){}
 
     template<typename T>
-    std::shared_ptr<T> LoadClass(uint32_t class_id_);
+    std::shared_ptr<T> LoadClass(uint32_t class_id_) {
+        m_job_handler = dlopen(COMMON_CONF->JOB_PATH.c_str(), RTLD_LAZY);
+        dlerror(); // clear error code
+        // find Job by job_id within the memory so
+        std::string job_symbol = "create_job_" + std::to_string(class_id_);
+        std::shared_ptr<T> (*create_job_fun)();
+        create_job_fun = (std::shared_ptr<T> (*)())dlsym(m_job_handler, job_symbol.c_str());
+        const char *dlsym_error = NULL;
+        if ((dlsym_error = dlerror()) != NULL){
+            //throw exception
+            return NULL;
+            //throw ErrorException(UNDEFINED_ENV_VARIABLE);
+        } else {
+            // create Job instance
+            return create_job_fun();
+        }
+    }
 
 private:
     void* m_job_handler;
