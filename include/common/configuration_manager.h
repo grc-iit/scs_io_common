c625b9927a4d36bb8c3abbb2a9a15113141f4066
Hariharan Devarajan <mani.hariharan@gmail.com>
2020-09-25 11:46:43 -0500

    fixed byteflow_regulator test for calculating inflow and outflow rate.

--- include/common/configuration_manager.h
+++ include/common/configuration_manager.h
@@ -136,8 +136,10 @@ namespace common {
                 int count;
                 while (getline(file, file_line)) {
                     if (!file_line.empty()) {
-                        int split_loc = file_line.find(" slots=");
-                        int split_loc2 = file_line.find('='); // split to node and net
+                        int split_loc = file_line.find(":");
+                        int split_loc2 = file_line.find(':'); // split to node and net
+                        //int split_loc = file_line.find(" slots=");
+                        //int split_loc2 = file_line.find('='); // split to node and net
                         if (split_loc != std::string::npos) {
                             server_node_name = file_line.substr(0, split_loc);
                             count = atoi(file_line.substr(split_loc2+1, std::string::npos).c_str());
